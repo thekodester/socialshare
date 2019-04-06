2144553fc3d097ce652062b2734f2f52db85116e
Kody <23484460+thekodester@users.noreply.github.com>
2019-04-06 02:22:10 -0400

    Fix bug with chrome

--- assets/js/socialshare.js
+++ assets/js/socialshare.js
@@ -345,9 +345,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
    */
   function verticalBtn( largeDevice ) {
     if ( largeDevice.matches ) {
-      document.getElementById( 'socialShareBtn' ).value = "S\r\nh\r\na\r\nr\r\ne\r\n\u27A5";
+      document.getElementById( 'socialShareBtnText' ).innerHTML = "S\r\nh\r\na\r\nr\r\ne\r\n\u27A5";
     } else {
-      document.getElementById( 'socialShareBtn' ).value = "\u27A5 Share";
+      document.getElementById( 'socialShareBtnText' ).innerHTML = "\u27A5 Share";
     }
   }
 
